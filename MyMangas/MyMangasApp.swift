a5a7647ee54e64c02276b554129de37ad89b0230
Carlos Lucas Sánchez <lukolukas@icloud.com>
2023-12-19 19:52:28 +0100

    Initial Commit

---
+++ MyMangas/MyMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMangasApp.swift
+//  MyMangas
+//
+//  Created by Carlos Lucas Sánchez on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
