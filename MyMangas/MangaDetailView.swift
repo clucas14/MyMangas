fdd36d804e78388ffc3c33b578e5161218f55eee
Carlos Lucas Sánchez <lukolukas@icloud.com>
2024-01-23 20:56:18 +0100

    Vista detalle con collapse

--- MyMangas/MangaDetailView.swift
+++ MyMangas/MangaDetailView.swift
@@ -8,46 +8,72 @@
 import SwiftUI
 
 struct MangaDetailView: View {
+    
+    @State var expandedAuthors = false
+    @State var expandedGenres = false
+    @State var expandedDemographics = false
+    @State var expandedThemes = false
+    
     var manga: Manga
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .center) {
-                MangaPictureView(manga: manga, size: .detailPicture)
-                MangaCircleScoreView(manga: manga)
-                //No lo puedo hacer con offset porque la animación se salta el offset y se va moviendo hasta el offset
-                //.offset(CGSize(width: 130, height: -395.0))
-                    .padding(.top, -385)
-                    .padding(.leading, 250)
-                HStack {
-                    Text("Título en japonés: ")
-                    Text(manga.titleJapanese ?? "")
-                }
-                VStack {
-                    Text("Autores: ")
-                    ForEach(manga.authors) {
-                        Text($0.fullName)
+//                ScrollView {
+                    List {
+                        VStack(alignment: .center) {
+                            MangaPictureView(manga: manga, size: .detailPicture)
+                            MangaCircleScoreView(manga: manga)
+                            //No lo puedo hacer con offset porque la animación se salta el offset y se va moviendo hasta el offset
+                            //.offset(CGSize(width: 130, height: -395.0))
+                                .padding(.top, -385)
+                                .padding(.leading, 250)
+                            HStack {
+                                Text("Título en japonés: ")
+                                Text(manga.titleJapanese ?? "")
+                            }
+                        }
+//            List {
+                Section("Authors: ", isExpanded: $expandedAuthors) {
+                    if !manga.authors.isEmpty {
+                        ForEach(manga.authors) {
+                            Text($0.fullName)
+                        }
+                    } else {
+                        Text("No information")
                     }
                 }
-                VStack {
-                    Text("Géneros: ")
-                    ForEach(manga.genres) {
-                        Text($0.genre.rawValue)
+                Section("Genres: ", isExpanded: $expandedGenres) {
+                    if !manga.genres.isEmpty {
+                        ForEach(manga.genres) {
+                            Text($0.genre.rawValue)
+                        }
+                    } else {
+                        Text("No information")
+                    }
+                }
+                Section("Demographics: ", isExpanded: $expandedDemographics) {
+                    if !manga.demographics.isEmpty {
+                        ForEach(manga.demographics) {
+                            Text($0.demographic.rawValue)
+                        }
+                    } else {
+                        Text("No information")
+                    }
+                }
+                Section("Themes: ", isExpanded: $expandedThemes) {
+                    if !manga.themes.isEmpty {
+                        ForEach(manga.themes) {
+                            Text($0.theme.rawValue)
+                        }
+                    } else {
+                        Text("No information")
                     }
                 }
             }
+            .listStyle(.sidebar)
             .navigationTitle(manga.title)
-//            .toolbar {
-//                ToolbarItem {
-//                    Button {
-//                        
-//                    } label: {
-//                        Text(manga.inCollection ? "Editar o eliminar" : "Añadir")
-//                    }
-//                }
-//            }
         }
-    }
+        
+//    }
 }
 
 #Preview {
